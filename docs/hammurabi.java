ef11393fd7ea2a662af6c7d0b6fbd72b730fdd0f
thejoeflynn <thejoeflynn@gmail.com>
2026-04-18 18:30:57 -0400

    made new file of hammurabi

---
+++ docs/hammurabi.java
@@ -0,0 +1,13 @@
+package hammurabi;
+import java.util.Random;
+import java.util.Scanner;
+
+public class hammurabi {
+    Random random = new Random();
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        new Hammurabi().playGame();
+    }
+
+}
